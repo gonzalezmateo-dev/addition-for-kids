fe2259b0adcc06ca6622a9331ce4db25671d9756
Mateo Gonzalez <gonzalezmateo.dev@gmail.com>
2024-03-14 16:25:43 -0500

    Added PrintNumberWithSpaces method

--- obj/Debug/net8.0/prueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4007c342c9dfcc724a29e1695e0c89bde9d621d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4501b25358ad3492e6041d36edd658c36864ec05")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- randomAddition.cs
+++ randomAddition.cs
@@ -1,41 +1,51 @@
-﻿Random rnd = new Random();
-String rndNumber;
-int result = 0;
-String sumando = " ";
+﻿using System;
 
-Console.WriteLine("\n Solve addition:");
-
-// Show random numbers
-for (int i = 0; i < 5; i++)
+class Program
 {
-    Console.Write("\n ");
-    for (int j = 0; j < 5; j++)
+    static void Main()
     {
-        rndNumber = rnd.Next(1,10).ToString();
-        sumando += rndNumber;
-        Console.Write(" " + rndNumber);
-    }
-    result += Int32.Parse(sumando);
-    sumando = "";
+        Random rnd = new Random();
+        
+        Console.Write("Enter number of numbers you want to generate: ");
+        int n = Convert.ToInt32(Console.ReadLine());
     
-
-}
-
-// Separate numbers
-// result mod 10 because 255 = 2*100 + 5 * 10 + 5
-int[] numAr = {0, 0, 0, 0, 0, 0};
-int cont = 5;
-while(result > 0){   
-    numAr[cont] = result % 10;
-    cont--;
-    result /= 10;
-}
-
-// Print orderned numbers 
-Console.WriteLine("\n\nPress any key to continue . . . ");
-Console.ReadKey(true);
-
-Console.WriteLine(" -----------");
-foreach(int k in numAr){
-    Console.Write(k+" ");
+        int[] randomNumbers = new int[n];
+        
+        // Generate and save random numbers in the array
+        for (int i = 0; i < n; i++)
+        {
+            randomNumbers[i] = rnd.Next(10000, 100000); // Generate a 5 digit random number
+        }
+        
+        // Print the random numbers stored in the array with spaces between their digits
+        
+        foreach (int number in randomNumbers)
+        {
+            Console.Write("  ");
+            PrintNumberWithSpaces(number);
+        }
+        
+        // Calculate the sum of all random numbers
+        int sum = 0;
+        foreach (int number in randomNumbers)
+        {
+            sum += number;
+        }
+        
+        // Imprimir la suma con espacios entre sus dígitos
+        Console.WriteLine("\nPress enter to know the addtion result:");
+        Console.ReadKey(true);
+        PrintNumberWithSpaces(sum);
+    }
+    
+    // Method to print a number with spaces between its digits
+    static void PrintNumberWithSpaces(int number)
+    {
+        string numberString = number.ToString();
+        foreach (char digit in numberString)
+        {
+            Console.Write(digit + " ");
+        }
+        Console.WriteLine(); 
+    }
 }
